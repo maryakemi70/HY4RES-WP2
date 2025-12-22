7da00ee3525a47e62b931610e4162113043be3d9
Mariana Akemi Ikegawa Bernabé <maryakemi70@gmail.com>
2025-12-22 14:17:38 +0100

    Refactor EnergySummary class with imports and methods

--- src/summary.py
+++ src/summary.py
@@ -37,7 +37,7 @@ class EnergySummary:
 
     def show_summary(self):
         # --- Title ---
-        st.markdown(f"<h2 style='text-align:center'>{self.title} LALALALALALAL</h2>", unsafe_allow_html=True)
+        st.markdown(f"<h2 style='text-align:center'>{self.title}</h2>", unsafe_allow_html=True)
         st.markdown(
             f"<h3 style='text-align:center'>{self.time_horizon_days }-day ahead cumulative total from {self.selected_date} </h3>",
             unsafe_allow_html=True
@@ -137,3 +137,4 @@ class EnergySummary:
                 unsafe_allow_html=True
 
             )
+
